ead031d9dbf710125b6ffce49142a4bb4a535b5b
MansillaLorena <lorenamansillag@gmail.com>
2024-09-18 21:29:20 +0000

    final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,45 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let randomNumber = Math.random() * 10;
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
+
   console.log("Hello Rigo from the console!");
+
+  function randomelement(arreglo) {
+    let arregloindex = Math.floor(Math.random() * arreglo.length);
+    return arreglo[arregloindex];
+  }
+
+  function generateExcuse() {
+    let who = ["A unicorn", "Wonder Woman", "A dancing penguin"];
+    let action = [
+      "did a somersault",
+      "sang opera",
+      "dressed up as a dinosaur",
+      "told a terrible joke"
+    ];
+    let what = [
+      "in the middle of the office",
+      "at the supermarket",
+      "during a conference"
+    ];
+    let when = [
+      "yesterday afternoon",
+      "Last weekend",
+      "five minutes ago",
+      "Last Monday"
+    ];
+
+    return (
+      randomelement(who) +
+      " " +
+      randomelement(action) +
+      " " +
+      randomelement(what) +
+      " " +
+      randomelement(when) +
+      ""
+    );
+  }
 };
